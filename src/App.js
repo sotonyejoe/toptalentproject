684faa6b490633d3cacdfd21743be71b0567a622
sotonyejoe <50546549+sotonyejoe@users.noreply.github.com>
2022-07-15 00:05:34 +0100

    Add files via upload
    my toptalent project of directory page

---
+++ src/App.js
@@ -0,0 +1,17 @@
+//import logo from './logo.svg';
+import "./App.scss";
+import Directory from './Components/Directory'
+//import Faq from "./Components/Faq";
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <Directory />
+       {/* <Faq /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Data.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+
+export const ProductsId = [
+   {id:01, name:"", img_url:"", category:"", description:"", Price:""} ,
+   {id:02, name:"", img_url:"", category:"", description:"", Price:""} ,
+   {id:03, name:"", img_url:"", category:"", description:"", Price:""} ,
+   {id:04, name:"", img_url:"", category:"", description:"", Price:""} ,
+   {id:05, name:"", img_url:"", category:"", description:"", Price:""} ,
+   {id:06, name:"", img_url:"", category:"", description:"", Price:""} ,
+   {id:07, name:"", img_url:"", category:"", description:"", Price:""} ,
+   {id:08, name:"", img_url:"", category:"", description:"", Price:""} ,
+   {id:09, name:"", img_url:"", category:"", description:"", Price:""} ,
+   {id:10, name:"", img_url:"", category:"", description:"", Price:""} ,
+   {id:11, name:"", img_url:"", category:"", description:"", Price:""} ,
+   {id:12, name:"", img_url:"", category:"", description:"", Price:""} ,
+   {id:13, name:"", img_url:"", category:"", description:"", Price:""} ,
+   {id:14, name:"", img_url:"", category:"", description:"", Price:""} ,
+   {id:15, name:"", img_url:"", category:"", description:"", Price:""} ,
+   {id:16, name:"", img_url:"", category:"", description:"", Price:""} ,
+   {id:17, name:"", img_url:"", category:"", description:"", Price:""} ,
+   {id:18, name:"", img_url:"", category:"", description:"", Price:""} ,
+   {id:19, name:"", img_url:"", category:"", description:"", Price:""} ,
+   {id:20, name:"", img_url:"", category:"", description:"", Price:""} ,
+
+
+
+]
+
+export const Services = [
+
+    
+]
